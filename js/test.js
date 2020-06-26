ec4559104d7d79cfd1390e69731e18cb2c2db244
mirandawhen <mirandan@alumni.cmu.edu>
2020-06-25 18:39:45 -0700

    test

---
+++ js/test.js
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+	var s ="melloworld";
+
+while (s!= "melloworld")
+{
+s=prompt("please enter your password");
+if (s=="melloworld")
+{
+window.location.href="index.html"; //page to redirect if password entered is correct
+
+}
+else
+{
+alert("Incorrect password-Try again");
+
+}
+}
+</script>
